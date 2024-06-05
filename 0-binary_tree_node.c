c68d9fa42ec3ddd80aeb55b85ea46f3df9f5fd3d
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-06-05 16:56:44 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,27 +1,25 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-
-#include <stddef.h>
-#include <stdlib.h>
+#include "binary_trees.h"
 
 /**
- * struct binary_tree_s - Binary tree node
+ * binary_tree_node - Create a binary tree node.
  *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * @parent: A pointer to the parent of the node.
+ * @value: The value to be puted on the new node.
+ *
+ * Return: If it fails - NULL else - a pointer to the new node.
  */
-struct binary_tree_s
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	binary_tree_t *new;
 
-typedef struct binary_tree_s binary_tree_t;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
-#endif /* BINARY_TREES_H */
+	return (new);
+}
